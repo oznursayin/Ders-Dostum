7b2866a2692186aa8011a96a03331516cbe1b3de
oznursayin <oznurozturk1@hotmail.com>
2025-06-21 13:59:17 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './styles.css'
-import { useState } from 'react'
+import "./styles.css"
+import { useState } from "react"
 
-export default function app() {
+export default function App() {
   const [flashCard, setFlashCard] = useState({
-    question: 'React nedir?',
-    choices: ["JavaScript framework'ü", 'JavaScript kütüphanesi'],
-    answer: 'JavaScript kütüphanesi.',
+    question: "React nedir?",
+    choices: ["JavaScript framework'ü", "JavaScript kütüphanesi"],
+    answer: "JavaScript kütüphanesi.",
     explanation: `Birinin framework diyebilme cüretini gösterdiğini duyarsanız, onu mümkün olduğunca bilgili bir şekilde düzeltmeniz, tercihen yanıtınıza " aslında..." diye başlamanız önemlidir.`,
+    flipped: false,
   })
 
   /* Challenge: 
@@ -20,29 +21,35 @@ export default function app() {
         3. Aynı kalıp sonraki tıklamalar için de tekrarlanmalıdır, böylece kullanıcı kartı istediği kadar ileri geri çevirmeye devam edebilir. 
 */
 
+  const handleClick = () => {
+    setFlashCard((pre) => ({ ...pre, flipped: !pre.flipped }))
+  }
   return (
     <div>
       <header>
-        <img src='./images/react.svg' />
+        <img src="./images/react.svg" />
         <h1> React Çalışma Arkadaşı </h1>
       </header>
 
       {/*-------Aşağıdaki div'i düzenleyin------------*/}
 
-      <div className='flash-card'>
+      <div
+        className={`flash-card  ${flashCard.flipped ? "flipped" : ""}`}
+        onClick={handleClick}
+      >
         {/*-------Yukarıdaki div'i düzenleyin------------*/}
 
-        <div className='flash-card-inner'>
-          <div className='flash-card-front'>
-            <p className='question'>{flashCard.question}</p>
-            <ol type='a'>
+        <div className="flash-card-inner">
+          <div className="flash-card-front">
+            <p className="question">{flashCard.question}</p>
+            <ol type="a">
               {flashCard.choices.map((choice) => (
                 <li key={crypto.randomUUID()}>{choice}</li>
               ))}
             </ol>
           </div>
-          <div className='flash-card-back'>
-            <p className='answer'>{flashCard.answer}</p>
+          <div className="flash-card-back">
+            <p className="answer">{flashCard.answer}</p>
             <p>{flashCard.explanation}</p>
           </div>
         </div>
